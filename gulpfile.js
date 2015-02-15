214f84888558491e3b9e2d459a4086614f282a1e
Nate Dunn <hello@natedunn.net>
2015-02-14 19:50:09 -0700

    Move un-minified css to dist folder

--- gulpfile.js
+++ gulpfile.js
@@ -23,7 +23,7 @@ gulp.task('styles', function() {
     .pipe(sourcemaps.init())
     .pipe(sass({ style: 'nested' }))
     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-    .pipe(gulp.dest('src/css/'))
+    .pipe(gulp.dest('dist/css/'))
     .pipe(rename({ suffix: '.min' }))
     .pipe(minifycss())
     .pipe(sourcemaps.write())
